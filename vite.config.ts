9d739848a0d91191b261ba4bde52a9db820dc7dc
Ankit Bodar <82100816+ankitbodar001@users.noreply.github.com>
2025-05-04 16:38:54 +0000

    fix for 404

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,20 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/my-portfolio', // <-- replace with your repo name
-  //plugins: [react()],
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/my-portfolio/', // <-- replace with your repo name
+  plugins: [react()],
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  // plugins: [
+  //   react(),
+  //   mode === 'development' &&
+  //   componentTagger(),
+  // ].filter(Boolean),
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 }));

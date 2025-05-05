1cffb081a5b9c0b0a9e371e547613d86b99d6624
Ankit Bodar <82100816+ankitbodar001@users.noreply.github.com>
2025-05-05 11:44:04 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,16 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/my-portfolio/', // <-- replace with your repo name
-  plugins: [react()],
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
-  // plugins: [
-  //   react(),
-  //   mode === 'development' &&
-  //   componentTagger(),
-  // ].filter(Boolean),
+  // plugins: [react()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
